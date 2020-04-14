ada7f9b3fe4e6d470f279b423bea116305dc2819
Aldhynesa Ardhana Said <38852597+aldhynesa@users.noreply.github.com>
2020-04-14 18:36:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transactions' :
 
 if(!strpos($_SERVER['REQUEST_URI'],'/charge')){
 	http_response_code(404);
-	echo "wrong path, make sure it's '/charge'";exit();
+	echo "wrong path, make sure it '/charge'"; exit();
 }
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
